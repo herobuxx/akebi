d5afe3a6c7771ed0fd085c2c1ddaa19e8a8be837
HeroBuxx <herobuxx@conqueros.co>
2022-02-24 19:19:36 +0700

    akebi: Install dependencies on ArchLinux or any distro with pacman
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 echo "*[+] Installing Dependecies for Akebi..."
-sudo DEBIAN_FRONTEND=noninteractive \
-    apt install \
-    x11-xserver-utils \
-    dnsutils \
-    wget -y
+
+if [[ "$(command -v apt)" != "" ]]; then
+ 	sudo DEBIAN_FRONTEND=noninteractive \
+	    apt install \
+    	x11-xserver-utils \
+    	dnsutils \
+    	wget -y
+elif [[ "$(command -v pacman)" != "" ]]; then
+   sudo pacman -Sy --noconfirm \
+        xorg-xrandr \
+        dig
+fi
 
 echo "*[+] Installing Akebi..."
 sudo make > /dev/null
